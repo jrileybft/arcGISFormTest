380f1a039e04a13cc106c6b689e5c2242e69f65a
jrileybft <31543934+jrileybft@users.noreply.github.com>
2023-11-22 15:56:12 -0500

    Update ArcGISGeocoderPlugin.js

--- ArcGISGeocoderPlugin.js
+++ ArcGISGeocoderPlugin.js
@@ -24,7 +24,7 @@ const fire = <T>(
   return event;
 };
 
-@customElement('arcgis-geocoder-plugin')
+@customElement('arcgis-geocoder-plugin2')
 export class ArcGISGeocoderPlugin extends LitElement {
   @property()
   address!: string;
